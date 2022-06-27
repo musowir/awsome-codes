28004a4a522d017424c9c6fae045263d9399a61b
Abdu Musowir U <blueiristech@gmail.com>
2022-06-27 11:14:10 +0530

    Update Caesarscipher.py

--- Caesarscipher.py
+++ Caesarscipher.py
@@ -1,16 +1,16 @@
-#Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
 
-#Original alphabet:      abcdefghijklmnopqrstuvwxyz
-#Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
 
-#Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
+Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
 
-#Sample Input
+Sample Input
 11
-#middle-Outz
+middle-Outz
 2
 
-#Sample Output
+Sample Output
 
 okffng-Qwvb
 
@@ -33,25 +33,7 @@ z -> b
 
 
 #code
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'caesarCipher' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING s
-#  2. INTEGER k
-#
-
 def caesarCipher(s, k):
-    # Write your code here
     r=[]
     for i in s:
         if((i.isalpha()) and (i.isupper())):
@@ -63,18 +45,3 @@ def caesarCipher(s, k):
     s = "".join(r)        
 
     return s      
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    s = input()
-
-    k = int(input().strip())
-
-    result = caesarCipher(s, k)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
